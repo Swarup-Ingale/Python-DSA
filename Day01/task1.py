a89ee8a06d89755cbcb45cfc01e680348edb53f8
Swarup Ingale <swarupingale68@gmail.com>
2026-09-08 18:48:58 +0530

    Create task1.py

---
+++ Day01/task1.py
@@ -0,0 +1,18 @@
+# Find Sum of digits
+# reverse a number
+# count number of digits 
+# check if no is palindrome or not
+# factorial without recursion
+
+#3
+
+# a = (input("enter the number : "))
+# b = len(a)
+# print(b)
+
+# 1
+a = int(input("enter the number : "))
+b = len(a)
+for i in range(b):
+    c = a % 10
+print(b)
